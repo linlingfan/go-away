5c4bb3750a6e5245beadd390f86b7cac6ec13d3d
linlingfan <876331806@qq.com>
2021-07-20 16:03:06 +0800

    save

--- interview/algorithm/code_top_test.go
+++ interview/algorithm/code_top_test.go
@@ -3,6 +3,8 @@ package algorithm
 import (
 	"fmt"
 	"math"
+	"strconv"
+	"strings"
 	"testing"
 )
 
@@ -100,10 +102,9 @@ func selectSort(nums [][]int) {
 	}
 }
 
-
 // ------
 func TestName(t *testing.T) {
-	m:=mySqrt(6)
+	m := mySqrt(6)
 	println(m)
 	x := math.Abs(-1.13)
 	println(x)
@@ -113,8 +114,8 @@ func mySqrt(x int) int {
 	l, r := 0, x
 	ans := -1
 	for l <= r {
-		mid := l + (r - l) / 2
-		if mid * mid <= x {
+		mid := l + (r-l)/2
+		if mid*mid <= x {
 			ans = mid
 			l = mid + 1
 		} else {
@@ -124,3 +125,140 @@ func mySqrt(x int) int {
 	return ans
 }
 
+// --------
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+var ans [][]int
+
+func pathSum(root *TreeNode, targetSum int) [][]int {
+	if root == nil {
+		return ans
+	}
+	ans = make([][]int, 0, 0)
+	var pathArr []int
+	dfs(root, targetSum, pathArr)
+	return ans
+}
+
+func dfs(node *TreeNode, targetSum int, pathArr []int) {
+	if node == nil {
+		return
+	}
+	if node.Left == nil && node.Right == nil {
+		pathArr = append(pathArr, node.Val)
+		sum := 0
+		for _, value := range pathArr {
+			sum = sum + value
+		}
+		if sum == targetSum {
+			var temp []int
+			copy(temp, pathArr)
+			ans = append(ans, pathArr)
+		}
+		return
+	}
+	pathArr = append(pathArr, node.Val)
+	dfs(node.Left, targetSum, pathArr)
+	dfs(node.Right, targetSum, pathArr)
+}
+
+func TestTreeNodeDFS(t *testing.T) {
+	root := &TreeNode{
+		Val: 1,
+	}
+	for i := 0; i < 4; i++ {
+
+	}
+	pathSum(root, 22)
+}
+
+func TestCopyArr(t *testing.T) {
+	arr := []int{1, 2, 4}
+	arrCopy := make([]int, len(arr))
+	copy(arrCopy, arr)
+	fmt.Printf("%+v \n", arrCopy)
+	fmt.Printf("%+v \n", arr)
+	fmt.Printf("%+v \n", arr[:len(arr)-1])
+}
+
+// -------
+// 滑动窗口
+// 窗口大小
+// 校验t是否全部在窗口
+
+func TestMinWindow(t *testing.T) {
+	n := math.MaxInt64
+	println(n)
+	// "ADOBECODEBANC"
+	//"ABC"
+	// ""brfwhqcbnvcwmesnzjrkfvsbnmwruvzxpewyxyzkwbiigawflprrmkuayqtunsscyqbvqahmepvkstrdkkuwwoyugrpuljwkzzhpapeggbsaujegvacsrdmxwbtiksulrbdtshzbirbechkykoqbreyqguasmugdxjzssytasweugquqemryrrozlqtzmzdjkedccyewvitdantfcmdtdturlpqxrvgbzmgekkchvvkvjfcnrfgxacqlrgiiermfdulpeoxbjhzunbhejpdjttxnchxgajnfdzvknsnyzmtoocvxuyinshaupycrjpxpouzdmmcnzxcpbxovvwjpgjlvjnotfnivhjzvvdjwcnndkbzhykzetkbfaryfficorbkogtniiamscnbcxaisnlxpcxujfwyyaipgdcrleffisimpluhyhfnmejvmhtkjdzsidjtioaqijdxuzeuhlhtssqebmolpqdatovdewumfzjbypvhdecvytbzfpkwhpwdfsgjqavxbgycyvjbzmyxhzymqlkachdibjrtmpqnxqpvlzymnyluzjiswsszbvheeaxsppgpfvkswlgljrhjdpbzktalqhqwippzfxomsbfrmnevfmgkdhlncmbuvtrfiifpfvczwjqiyxgfjmpowdearfvmymsiuzazuearprbnqjtbwkpu"
+	//"ntugap"
+	str := "brfwhqcbnvcwmesnzjrkfvsbnmwruvzxpewyxyzkwbiigawflprrmkuayqtunsscyqbvqahmepvkstrdkkuwwoyugrpuljwkzzhpapeggbsaujegvacsrdmxwbtiksulrbdtshzbirbechkykoqbreyqguasmugdxjzssytasweugquqemryrrozlqtzmzdjkedccyewvitdantfcmdtdturlpqxrvgbzmgekkchvvkvjfcnrfgxacqlrgiiermfdulpeoxbjhzunbhejpdjttxnchxgajnfdzvknsnyzmtoocvxuyinshaupycrjpxpouzdmmcnzxcpbxovvwjpgjlvjnotfnivhjzvvdjwcnndkbzhykzetkbfaryfficorbkogtniiamscnbcxaisnlxpcxujfwyyaipgdcrleffisimpluhyhfnmejvmhtkjdzsidjtioaqijdxuzeuhlhtssqebmolpqdatovdewumfzjbypvhdecvytbzfpkwhpwdfsgjqavxbgycyvjbzmyxhzymqlkachdibjrtmpqnxqpvlzymnyluzjiswsszbvheeaxsppgpfvkswlgljrhjdpbzktalqhqwippzfxomsbfrmnevfmgkdhlncmbuvtrfiifpfvczwjqiyxgfjmpowdearfvmymsiuzazuearprbnqjtbwkpu"
+	t1 := "ntugap"
+	s := minWindow(str, t1)
+	println(s)
+}
+
+func minWindow(s string, t string) string {
+	windowLen := len(t)
+	sLen := len(s)
+	if sLen == 0 || sLen < windowLen {
+		return ""
+	}
+	tMap := make(map[string]int, 0)
+	for _, v := range t {
+		_, ok := tMap[string(v)]
+		if ok {
+			tMap[string(v)] = tMap[string(v)] + 1
+		} else {
+			tMap[string(v)] = 1
+		}
+	}
+	isHad := false
+	windowRight := windowLen
+	for windowRight <= sLen && windowLen <= sLen {
+		sl := s[windowRight-windowLen : windowRight]
+		sMap := make(map[string]int, 0)
+		for _, v := range sl {
+			_, ok := sMap[string(v)]
+			if ok {
+				sMap[string(v)] = sMap[string(v)] + 1
+			} else {
+				sMap[string(v)] = 1
+			}
+		}
+		for key, v := range tMap {
+			if strings.Contains(sl, key) && sMap[key] >= v {
+				isHad = true
+			} else {
+				isHad = false
+				break
+			}
+		}
+		if isHad {
+			return sl
+		}
+
+		if windowRight == sLen {
+			windowLen++
+			windowRight = windowLen
+		} else {
+			windowRight++
+		}
+	}
+	return ""
+}
+
+// ----- version
+
+func TestCompareVersion(t *testing.T) {
+	num, err := strconv.Atoi("001")
+	if err != nil {
+		fmt.Printf("%+v \n", err)
+	}
+	println(num)
+}
