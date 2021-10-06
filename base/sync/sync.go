ec6a03f2b68195b1c3fdc9b68777e7572785fe58
linlingfan <876331806@qq.com>
2021-10-06 22:34:35 +0800

    merge and fix

--- base/sync/sync.go
+++ base/sync/sync.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"context"
-	"errors"
 	"fmt"
-	"golang.org/x/sync/errgroup"
 	"sync"
 	"time"
 )
@@ -36,31 +34,87 @@ func main() {
 	//wg2.Wait()
 
 	// 协程组异常终止其他协程 errgroup 的使用
-	group, c := errgroup.WithContext(context.Background())
-	//var group errgroup.Group
-	group.Go(func() error {
-		time.Sleep(time.Second * 2)
-		select {
-		case <-c.Done(): // 通过
-			fmt.Println("get ctx cancel and done!")
-			return nil
-		default:
-			fmt.Println("do go...")
-		}
-		return nil
-	})
-	group.Go(func() error {
-		return errors.New("123")
-	})
-	group.Go(func() error {
-		time.Sleep(time.Second)
-		return errors.New("321")
-	})
-	err := group.Wait()
-	if err != nil {
-		fmt.Printf("%+v", err)
-	}
+	//group, c := errgroup.WithContext(context.Background())
+	////var group errgroup.Group
+	//group.Go(func() error {
+	//	time.Sleep(time.Second * 2)
+	//	select {
+	//	case <-c.Done(): // 通过
+	//		fmt.Println("get ctx cancel and done!")
+	//		return nil
+	//	default:
+	//		fmt.Println("do go...")
+	//	}
+	//	return nil
+	//})
+	//group.Go(func() error {
+	//	return errors.New("123")
+	//})
+	//group.Go(func() error {
+	//	time.Sleep(time.Second)
+	//	return errors.New("321")
+	//})
+	//err := group.Wait()
+	//if err != nil {
+	//	fmt.Printf("%+v", err)
+	//}
 
+	// 交替输出
+
+	//msg := make(chan int)
+	//go groutine1(msg)
+	//go groutine2(msg)
+	//time.Sleep(time.Second * 1)
+
+	// 同步
+	//var mu sync.Mutex
+	//
+	//mu.Lock()
+	//go func(){
+	//	println("你好, 世界")
+	//	mu.Unlock()
+	//}()
+	//mu.Lock()
+
+	// 使用无缓存管道 实现同步
+	//ch := make(chan int)
+	//go func() {
+	//	time.Sleep(time.Second*1)
+	//	ch <- 1
+	//	println("123")
+	//}()
+	//<-ch
+	//println(321)
+
+	// 协程组异常 终止其他协程组
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	var wg3 sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg3.Add(1)
+		id := i
+		go func() {
+			defer wg3.Done()
+			select {
+			case <-ctx.Done():
+				fmt.Println("id:%d stop!", id)
+				return
+			default:
+				// 正常执行
+				//fmt.Println("id:%d work3 begin", id)
+			}
+			if id == 5 {
+				cancel()
+				fmt.Printf("error return %d \n",id)
+				return
+			} else {
+				println(id)
+				time.Sleep(time.Second * 5)
+			}
+		}()
+	}
+	time.Sleep(time.Second)
+	cancel()
+	wg3.Wait()
 }
 
 // 协程组安全通知退出
@@ -97,3 +151,23 @@ func work2(ctx context.Context, id int, wg *sync.WaitGroup) {
 }
 
 // 手动实现一个异常返回的协程组 参照 errGroup
+
+//
+var POOL = 100
+
+func groutine1(p chan int) {
+	for i := 1; i <= POOL; i++ {
+		p <- i
+		if i%2 == 1 {
+			fmt.Println("groutine-1:", i)
+		}
+	}
+}
+func groutine2(p chan int) {
+	for i := 1; i <= POOL; i++ {
+		<-p
+		if i%2 == 0 {
+			fmt.Println("groutine-2:", i)
+		}
+	}
+}
--- main.go
+++ main.go
@@ -1,6 +1,64 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+)
+
 func main() {
 
+	var wg sync.WaitGroup
+	//errgroup, c := errgroup.WithContext(context.Background())
+	ctx, _ := context.WithTimeout(context.Background(), time.Second*2)
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		id := i
+		go work(ctx, &wg, id)
+		//errgroup.Go(func() error {
+		//	if id == 2 {
+		//		time.Sleep(time.Second * 3)
+		//		return errors.New("超时")
+		//	}
+		//	println(id)
+		//	return nil
+		//})
 
+	}
+
+	select {
+	case <-ctx.Done():
+		fmt.Printf("超时:%+v", ctx.Err())
+	}
+
+	wg.Wait()
+	//err := errgroup.Wait()
+	//if err != nil {
+	//	fmt.Printf("%+v", err)
+	//}
+}
+
+func work(ctx context.Context, wg *sync.WaitGroup, id int) {
+	//func work(ctx context.Context, id int) {
+	defer func() {
+		err := recover()
+		if err != nil {
+			fmt.Printf("%+v", err)
+		}
+		wg.Done()
+	}()
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			// 执行逻辑
+			if id == 2 {
+				time.Sleep(time.Second * 3)
+			}
+			println(id)
+			return
+		}
+	}
 }
